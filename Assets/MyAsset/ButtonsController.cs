3b0ea5fa19bb5e9b5a969033f2474055c5659a4c
devina-ekawati <devina.ekawati@gmail.com>
2017-12-17 20:55:25 +0800

    Init project, add main scene

---
+++ Assets/MyAsset/ButtonsController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HUX.Focus;
+using HUX.Interaction;
+using HUX.Receivers;
+
+namespace HUX
+{
+    public class ButtonsController : InteractionReceiver
+    {
+        public GameObject TextObjectState;
+        public TextMesh txt;
+
+        // Use this for initialization
+        void Start()
+        {
+            txt = TextObjectState.GetComponentInChildren<TextMesh>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        protected override void OnTapped(GameObject obj, InteractionManager.InteractionEventArgs eventArgs)
+        {
+            txt.text = obj.name + " : OnTapped";
+
+            switch (obj.name)
+            {
+                case "SpecialRouteButtonHolographic":
+                    // Do something on ButtonHolographic:OnTapped
+                    break;
+                case "JustStartButtonHolographic":
+                    // Do something on ButtonHolographic:OnTapped
+                    break;
+                case "ProfesionalRouteButtonHolographic":
+                    // Do something on ButtonHolographic:OnTapped
+                    break;
+            }
+            base.OnTapped(obj, eventArgs);
+        }
+    }
+}
