37d70c9a6cf2d22748d2ee89ce95c2a1549f84d4
devina-ekawati <devina.ekawati@gmail.com>
2017-12-19 00:53:18 +0800

    Change buttons UI, add login scene

---
+++ Assets/MyAsset/Script/ButtonsController.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HUX.Focus;
+using HUX.Interaction;
+using HUX.Receivers;
+
+namespace HUX
+{
+    public class ButtonsController : InteractionReceiver
+    {
+        public GameObject TextObjectState;
+        public TextMesh txt;
+
+        public Sprite SpecialRouteButtonClicked, SpecialRouteButtonDefault;
+        public Sprite JustStartButtonClicked, JustStartButtonDefault;
+        public Sprite ProfessionalButtonClicked, ProfessionalButtonDefault;
+
+        // Use this for initialization
+        void Start()
+        {
+            txt = TextObjectState.GetComponentInChildren<TextMesh>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        protected override void OnTapped(GameObject obj, InteractionManager.InteractionEventArgs eventArgs)
+        {
+            txt.text = obj.name + " : OnTapped";
+
+            switch (obj.name)
+            {
+                case "SpecialRouteButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = SpecialRouteButtonClicked;
+                    break;
+                case "JustStartButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = JustStartButtonClicked;
+                    break;
+                case "ProfessionalRouteButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = ProfessionalButtonClicked;
+                    break;
+            }
+
+            base.OnTapped(obj, eventArgs);
+        }
+
+        protected override void OnHoldStarted(GameObject obj, InteractionManager.InteractionEventArgs eventArgs)
+        {
+            txt.text = obj.name + " : OnHoldStarted";
+
+            switch (obj.name)
+            {
+                case "SpecialRouteButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = SpecialRouteButtonClicked;
+                    break;
+                case "JustStartButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = JustStartButtonClicked;
+                    break;
+                case "ProfessionalRouteButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = ProfessionalButtonClicked;
+                    break;
+            }
+
+            base.OnHoldStarted(obj, eventArgs);
+        }
+
+        protected override void OnFocusEnter(GameObject obj, FocusArgs args)
+        {
+            txt.text = obj.name + " : OnFocusEnter";
+
+            switch (obj.name)
+            {
+                case "SpecialRouteButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = SpecialRouteButtonClicked;
+                    break;
+                case "JustStartButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = JustStartButtonClicked;
+                    break;
+                case "ProfessionalRouteButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = ProfessionalButtonClicked;
+                    break;
+            }
+
+            base.OnFocusEnter(obj, args);
+        }
+
+        protected override void OnFocusExit(GameObject obj, FocusArgs args)
+        {
+            txt.text = obj.name + " : OnFocusExit";
+
+            switch (obj.name)
+            {
+                case "SpecialRouteButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = SpecialRouteButtonDefault;
+                    break;
+                case "JustStartButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = JustStartButtonDefault;
+                    break;
+                case "ProfessionalRouteButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = ProfessionalButtonDefault;
+                    break;
+            }
+
+            base.OnFocusExit(obj, args);
+        }
+    }
+}
---
+++ Assets/MyAsset/Script/LoginController.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HUX.Focus;
+using HUX.Interaction;
+using HUX.Receivers;
+
+namespace HUX
+{
+    public class LoginController : InteractionReceiver
+    {
+
+        public Sprite LoginButtonClicked, LoginButtonDefault;
+        public Sprite SignUpButtonClicked, SignUpButtonDefault;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        protected override void OnTapped(GameObject obj, InteractionManager.InteractionEventArgs eventArgs)
+        {
+
+            switch (obj.name)
+            {
+                case "LoginButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = LoginButtonClicked;
+                    break;
+                case "SignUpButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = SignUpButtonClicked;
+                    break;
+            }
+
+            base.OnTapped(obj, eventArgs);
+        }
+
+        protected override void OnHoldStarted(GameObject obj, InteractionManager.InteractionEventArgs eventArgs)
+        {
+
+            switch (obj.name)
+            {
+                case "LoginButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = LoginButtonClicked;
+                    break;
+                case "SignUpButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = SignUpButtonClicked;
+                    break;
+            }
+
+            base.OnHoldStarted(obj, eventArgs);
+        }
+
+        protected override void OnFocusEnter(GameObject obj, FocusArgs args)
+        {
+
+            switch (obj.name)
+            {
+                case "LoginButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = LoginButtonClicked;
+                    break;
+                case "SignUpButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = SignUpButtonClicked;
+                    break;
+            }
+
+            base.OnFocusEnter(obj, args);
+        }
+
+        protected override void OnFocusExit(GameObject obj, FocusArgs args)
+        {
+
+            switch (obj.name)
+            {
+                case "LoginButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = LoginButtonDefault;
+                    break;
+                case "SignUpButton":
+                    obj.GetComponentInChildren<SpriteRenderer>().sprite = SignUpButtonDefault;
+                    break;
+            }
+
+            base.OnFocusExit(obj, args);
+        }
+    }
+}
+
